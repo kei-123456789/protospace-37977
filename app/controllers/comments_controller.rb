940f6f1dbd3c8912034d50fc6188a5ca8ab60372
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-05-12 11:06:32 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
-    # if @comment.save
+    if @comment.save
       redirect_to prototype_path(@comment.prototype)
-    # else 
-    #   render "prototypes/show"
-    # end
+    else 
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show"
+    end
   end
 
   private
